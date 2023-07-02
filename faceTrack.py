5bfbf06efacbb34bd837705eb09859163628a0f0
蔡 炘晏 <71438514+colenator123-design@users.noreply.github.com>
2023-07-02 21:44:33 +0800

    Update faceTrack.py

--- faceTrack.py
+++ faceTrack.py
@@ -3,29 +3,32 @@ import cv2
 import socket
 import mediapipe as mp
 
-cap=cv2.VideoCapture(0)
-cap.set(3,640)
-cap.set(4,480)
-detector=FaceDetector(minDetectionCon=0.8)
-sock=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-serverAddressPort=('127.0.0.1',5052)
+cap = cv2.VideoCapture(0)
+cap.set(3, 640)
+cap.set(4, 480)
+detector = FaceDetector(minDetectionCon=0.8)
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+serverAddressPort = ('127.0.0.1', 5052)
 dZ = 3.0
 mp_face_mesh = mp.solutions.face_mesh
-# 初始化Face Mesh模型
+# 初始化 Face Mesh 模型
 face_mesh = mp_face_mesh.FaceMesh()
 
 while True:
-    success,img=cap.read()
-    img = cv2.flip(img, flipCode=1) # 左右翻轉圖像
+    success, img = cap.read()
+    img = cv2.flip(img, flipCode=1)  # 左右翻轉圖像
     img, bboxs = detector.findFaces(img)
-    
+
     results = face_mesh.process(img)
     if bboxs:
         # bboxInfo - "id","bbox","score","center"
         center = bboxs[0]["center"]
-        print(center)        
-        data=str.encode(str(center))
-        sock.sendto(data,serverAddressPort)
+        print(center)
+        data = center + (dZ,) 
+        print(data)
+        data = str.encode(str(data))
+        sock.sendto(data, serverAddressPort)
+
     if results.multi_face_landmarks:
         # 取得第一個偵測到的臉部關鍵點
         face_landmarks = results.multi_face_landmarks[0]
@@ -45,21 +48,19 @@ while True:
         right_eye_x, right_eye_y = int(right_eye_coords.x * image_cols), int(right_eye_coords.y * image_rows)
         left_x, left_y = int(left_coords.x * image_cols), int(left_coords.y * image_rows)
         right_x, right_y = int(right_coords.x * image_cols), int(right_coords.y * image_rows)
-        #計算距離
+        # 計算距離
         width = image_cols
-        height = image_rows 
+        height = image_rows
         dx = left_x - left_eye_x
         dX = 3.5
         normalizedFocaleX = 1.40625
         fx = min(width, height) * normalizedFocaleX
-        dZ = (fx * (dX / dx))   
-        print("dZ:")
-        print(dZ)         
+        dZ = int(fx * (dX / dx))  # 取整數部分
 
-    cv2.imshow("image",img)
-    if cv2.waitKey(1) == 27: # 按下 ESC 鍵退出程序並釋放攝像頭
+    cv2.imshow("image", img)
+    if cv2.waitKey(1) == 27:  # 按下 ESC 鍵退出程序並釋放攝像頭
         break
 
 cap.release()  # 釋放攝像頭
 cv2.destroyAllWindows()  # 關閉所有視窗
-    
+
