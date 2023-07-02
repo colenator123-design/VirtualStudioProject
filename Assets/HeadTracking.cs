bfce8ed602144e8ae2741ffd593d18dc3e6c3aa7
ShiveyVer <73120244+ShiveyVer@users.noreply.github.com>
2023-07-02 22:59:06 +0800

    feat: depth add to unity, rotation cancelled

--- Assets/HeadTracking.cs
+++ Assets/HeadTracking.cs
@@ -10,6 +10,7 @@ public class HeadTracking : MonoBehaviour
     public GameObject cameraObject;
     List<float> xList = new List<float>();
     List<float> yList = new List<float>();
+    List<float> zList = new List<float>();
  
  
     // Start is called before the first frame update
@@ -32,22 +33,27 @@ public class HeadTracking : MonoBehaviour
  
         float x = (float.Parse(points[0])-320) / 100;
         float y = (float.Parse(points[1])-240) / 100;
+        float z = (float.Parse(points[2]) ) / 30 ;
+        Debug.Log(z);
         xList.Add(x);
         yList.Add(y);
+        zList.Add(z);
  
         if (xList.Count > 50) { xList.RemoveAt(0); }
         if (yList.Count > 50) { yList.RemoveAt(0); }
+        if (zList.Count > 50) { zList.RemoveAt(0); }
  
         float xAverage = Queryable.Average(xList.AsQueryable());
         float yAverage = Queryable.Average(yList.AsQueryable());
+        float zAverage = Queryable.Average(zList.AsQueryable());
  
  
         Vector3 cameraPos = cameraObject.transform.localPosition;
-        Vector3 cameraRot = cameraObject.transform.localPosition;
+        // Vector3 cameraRot = cameraObject.transform.localPosition;
  
  
-        cameraObject.transform.localPosition = new Vector3(-26.75f- xAverage, 2.9f- yAverage, cameraPos.z);
-        cameraObject.transform.localEulerAngles = new Vector3(18.76f- yAverage * 10, xAverage * 10 , 0);
+        cameraObject.transform.localPosition = new Vector3(-23f - xAverage, 1.3f- yAverage, 11 + zAverage);
+        // cameraObject.transform.localEulerAngles = new Vector3(18.76f- yAverage * 10, xAverage * 10 , 0);
  
     }
 }
