d6c88172039c6718610467829d1bc1df8315260f
ShiveyVer <73120244+ShiveyVer@users.noreply.github.com>
2023-09-19 17:54:55 +0800

    fix: code refactor

--- faceTrack.py
+++ faceTrack.py
@@ -3,64 +3,86 @@ import cv2
 import socket
 import mediapipe as mp
 
+# Configs
+image_rows = 480
+image_cols = 640
 cap = cv2.VideoCapture(0)
-cap.set(3, 640)
-cap.set(4, 480)
+cap.set(3, image_cols)
+cap.set(4, image_rows)
 detector = FaceDetector(minDetectionCon=0.8)
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 serverAddressPort = ('127.0.0.1', 5052)
-dZ = 3.0
+
 mp_face_mesh = mp.solutions.face_mesh
 # 初始化 Face Mesh 模型
 face_mesh = mp_face_mesh.FaceMesh()
 
-while True:
-    success, img = cap.read()
-    img = cv2.flip(img, flipCode=1)  # 左右翻轉圖像
-    img, bboxs = detector.findFaces(img)
 
-    results = face_mesh.process(img)
+class Coord:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+# get face's coord on screen by FaceDetector
+# return a tuple (x, y) on the scale of pixel
+def getCenter(img):
+    img, bboxs = detector.findFaces(img)
     if bboxs:
         # bboxInfo - "id","bbox","score","center"
-        center = bboxs[0]["center"]
-        print(center)
-        data = center + (dZ,) 
-        print(data)
-        data = str.encode(str(data))
-        sock.sendto(data, serverAddressPort)
+        return bboxs[0]["center"]
+    return (-1, -1)
 
+
+# get depth of face
+# return a integer on the scale of cm
+def getDepth(img):
+    results = face_mesh.process(img)
     if results.multi_face_landmarks:
         # 取得第一個偵測到的臉部關鍵點
         face_landmarks = results.multi_face_landmarks[0]
         # 取得眼睛的關鍵點索引
-        left_eye_landmark_index = 362
-        left = 359
-        right_eye_landmark_index = 133
-        right = 130
+        left_eye_l_index = 362
+        left_eye_r_index = 359
+        right_eye_l_index = 133
+        right_eye_r_index = 130
         # 取得左眼和右眼的座標
-        left_eye_coords = face_landmarks.landmark[left_eye_landmark_index]
-        left_coords = face_landmarks.landmark[left]
-        right_eye_coords = face_landmarks.landmark[right_eye_landmark_index]
-        right_coords = face_landmarks.landmark[right]
+        left_eye_l = face_landmarks.landmark[left_eye_l_index]
+        left_eye_r = face_landmarks.landmark[left_eye_r_index]
+        right_eye_l = face_landmarks.landmark[right_eye_l_index]
+        right_eye_r = face_landmarks.landmark[right_eye_r_index]
         # 將座標轉換為畫面上的位置
-        image_rows, image_cols, _ = img.shape
-        left_eye_x, left_eye_y = int(left_eye_coords.x * image_cols), int(left_eye_coords.y * image_rows)
-        right_eye_x, right_eye_y = int(right_eye_coords.x * image_cols), int(right_eye_coords.y * image_rows)
-        left_x, left_y = int(left_coords.x * image_cols), int(left_coords.y * image_rows)
-        right_x, right_y = int(right_coords.x * image_cols), int(right_coords.y * image_rows)
+        left_eye_l = Coord(left_eye_l.x * image_cols,
+                           left_eye_l.y * image_rows)
+        left_eye_r = Coord(left_eye_r.x * image_cols,
+                           left_eye_r.y * image_rows)
+        right_eye_l = Coord(right_eye_l.x * image_cols,
+                            right_eye_l.y * image_rows)
+        right_eye_r = Coord(right_eye_r.x * image_cols,
+                            right_eye_r.y * image_rows)
         # 計算距離
-        width = image_cols
-        height = image_rows
-        dx = left_x - left_eye_x
+        dx = left_eye_r.x - left_eye_l.x
         dX = 3.5
         normalizedFocaleX = 1.40625
-        fx = min(width, height) * normalizedFocaleX
-        dZ = int(fx * (dX / dx))  # 取整數部分
+        fx = min(image_cols, image_rows) * normalizedFocaleX
+        return int(fx * (dX / dx))
+        # dZ = int(fx * (dX / dx))
 
-    cv2.imshow("image", img)
-    if cv2.waitKey(1) == 27:  # 按下 ESC 鍵退出程序並釋放攝像頭
-        break
 
-cap.release()  # 釋放攝像頭
-cv2.destroyAllWindows()  # 關閉所有視窗
+if __name__ == '__main__':
+    while True:
+        success, image = cap.read()
+        image = cv2.flip(image, flipCode=1)  # 左右翻轉圖像
 
+        coord_data = getCenter(image) + (getDepth(image), )
+        # getAxis(image)
+        # print(coord_data)
+        data = str.encode(str(coord_data))
+        sock.sendto(data, serverAddressPort)
+
+        cv2.imshow("image", image)
+        if cv2.waitKey(1) == 27:  # press ESC to quit
+            break
+    # stop capturing and shut down
+    cap.release()
+    cv2.destroyAllWindows()
