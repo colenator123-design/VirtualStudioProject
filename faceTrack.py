563fd7b40a4ff9d8b1f9ada10248868fb460281f
ShiveyVer <73120244+ShiveyVer@users.noreply.github.com>
2023-09-19 17:55:43 +0800

    feat: dectecting rotations

--- faceTrack.py
+++ faceTrack.py
@@ -1,7 +1,9 @@
+import math
 from cvzone.FaceDetectionModule import FaceDetector
 import cv2
 import socket
 import mediapipe as mp
+import numpy as np
 
 # Configs
 image_rows = 480
@@ -69,13 +71,115 @@ def getDepth(img):
         # dZ = int(fx * (dX / dx))
 
 
+def getAxis(img):
+    # setup sampling landmarks
+    indexes = [1, 33, 263, 61, 291, 199]
+    objPts = np.array([[0.4941680431365967, 0.6710584759712219, -0.08302542567253113],
+                       [0.3756912052631378, 0.5486021041870117, 0.025898095220327377],
+                       [0.6125413179397583, 0.5426622629165649, 0.022610986605286598],
+                       [0.43580639362335205, 0.7722113132476807,
+                           0.0031681121326982975],
+                       [0.5561882257461548, 0.7670935988426208,
+                           0.0017560963751748204],
+                       [0.49711552262306213, 0.8604360818862915, -0.007275797426700592]
+                       ])
+    imgPts = []
+    # process through mediapipe and get face mesh
+    results = face_mesh.process(img)
+    if results.multi_face_landmarks is None:
+        print("No faces detected")
+        return
+    face_landmarks = results.multi_face_landmarks[0]
+    # print("----------")
+    # update coords of 6 sampling points
+    for i in indexes:
+        # print([face_landmarks.landmark[i].x,
+        #         face_landmarks.landmark[i].y, face_landmarks.landmark[i].z])
+        imgPts.append([face_landmarks.landmark[i].x,
+                       face_landmarks.landmark[i].y])
+    imgPts = np.array(imgPts)
+    # setup camera matrix
+    normalizedFocaleY = 1.28  # Logitech 922
+    focalLength = image_rows * normalizedFocaleY
+    camMat = np.array([[focalLength, 0, image_cols / 2.0],
+                       [0, focalLength, image_rows / 2.0],
+                       [0, 0, 1]])
+    # get rotation matrix, transform matrix by comparing inital coords (3D) to updated coords (on image, 2D)
+    # then decompose Euler angles from rotation matrix
+    success, rvec, tvec = cv2.solvePnP(objPts, imgPts, camMat, None)
+    if (not success):
+        print("sth went run with solvePnP")
+        return
+    rmat, _ = cv2.Rodrigues(rvec)
+    sy = math.sqrt(rmat[0, 0] * rmat[0, 0] + rmat[1, 0] * rmat[1, 0])
+
+    singular = sy < 1e-6
+
+    if not singular:
+        x = math.atan2(rmat[2, 1], rmat[2, 2])
+        y = math.atan2(-rmat[2, 0], sy)
+        z = math.atan2(rmat[1, 0], rmat[0, 0])
+    else:
+        x = math.atan2(-rmat[1, 2], rmat[1, 1])
+        y = math.atan2(-rmat[2, 0], sy)
+        z = 0
+    # print(x / 3.14 * 180,y/ 3.14 * 180,z/ 3.14 * 180)
+    # roll = y
+    # pitch = x
+    # yaw = z
+
+    # extend landmark1 (nose) by 0.2 on x, y, z, to make it 3 basis
+    worldPts = np.array([objPts[0], objPts[0], objPts[0]])
+    worldPts[0][0] += 0.2
+    worldPts[1][1] += 0.2
+    worldPts[2][2] += 0.2
+    worldPts = np.append(worldPts, objPts, axis=0)
+    # project the basis and draw it
+    imagePointsProjected, _ = cv2.projectPoints(
+        worldPts, rvec, tvec, camMat, None)
+    imagePointsProjected = imagePointsProjected.reshape(-1, 2)
+    drawAxis(img, imagePointsProjected)
+    return
+
+
+# show the axis calculated fom getAxis()
+# red one as x-axis, green one as y-axis, blue as z-axis
+def drawAxis(img, projectedPts):
+    line_width = 3
+    red = (0, 0, 255)
+    green = (0, 255, 0)
+    blue = (255, 0, 0)
+    # x-axis
+    cv2.line(img,
+             (int(projectedPts[3][0] * image_cols),
+              int(projectedPts[3][1] * image_rows)),
+             (int(projectedPts[0][0] * image_cols),
+              int(projectedPts[0][1] * image_rows)),
+             red, line_width)
+    # y-axis
+    cv2.line(img,
+             (int(projectedPts[3][0] * image_cols),
+              int(projectedPts[3][1] * image_rows)),
+             (int(projectedPts[1][0] * image_cols),
+                 int(projectedPts[1][1] * image_rows)),
+             green, line_width)
+    # z-axis
+    cv2.line(img,
+             (int(projectedPts[3][0] * image_cols),
+              int(projectedPts[3][1] * image_rows)),
+             (int(projectedPts[2][0] * image_cols),
+                 int(projectedPts[2][1] * image_rows)),
+             blue, line_width)
+    return
+
+
 if __name__ == '__main__':
     while True:
         success, image = cap.read()
         image = cv2.flip(image, flipCode=1)  # 左右翻轉圖像
 
         coord_data = getCenter(image) + (getDepth(image), )
-        # getAxis(image)
+        getAxis(image)
         # print(coord_data)
         data = str.encode(str(coord_data))
         sock.sendto(data, serverAddressPort)
